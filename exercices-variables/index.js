a26a511f28305b88fa3714e91b2c84d6f1f7a464
thomasR35 <thomasriou.35@gmail.com>
2024-11-20 10:21:27 +0100

    First js commit

---
+++ exercices-variables/index.js
@@ -0,0 +1,17 @@
+let name = "Legolas";
+console.log(character);
+
+name = "Gimli";
+console.log(character);
+
+let nombre1 = 10654;
+console.log(nombre1);
+let nombre2 = 1.2345;
+console.log(nombre2);
+let nombre3 = -12548;
+console.log(nombre3);
+
+let yes = true;
+console.log(yes);
+let no = false;
+console.log(no);
