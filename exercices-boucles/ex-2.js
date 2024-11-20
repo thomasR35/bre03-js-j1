604a70e82dd140b8738e946a9e55ccaa750cf5bb
thomasR35 <thomasriou.35@gmail.com>
2024-11-20 13:49:22 +0100

    Update ex-2.js

--- exercices-boucles/ex-2.js
+++ exercices-boucles/ex-2.js
@@ -2,10 +2,10 @@ let i = 0;
 
 while (i <= 10) {
   console.log(i);
-  i = i + 1;
   if (i % 2 === 1) {
     console.log("le chiffre est impair");
   } else {
     console.log("le chiffre est pair");
   }
+  i = i + 1;
 }
